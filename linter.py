fe59d0968b992a00f48a0c33bc19677c59cfdc49
Moritz Drexl <mdrexl@fastmail.fm>
2015-08-25 14:08:44 +0200

    first working version using gulp and all files

---
+++ linter.py
@@ -0,0 +1,35 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Moritz Drexl
+# Copyright (c) 2015 Moritz Drexl
+#
+# License: MIT
+#
+
+"""This module exports the Psc plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Psc(Linter):
+
+    """Provides an interface to psc."""
+
+    syntax = 'purescript'
+    cmd = 'gulp make --@'
+    regex = (
+        r'Error in module (?P<module>\S+)\nError at (\S+) line (?P<line>\d+), column (?P<col>\d+)(.+)\n  (?P<message>.+)'
+    )
+    multiline = True
+    tempfile_suffix = '-'
+    error_stream = util.STREAM_STDOUT
+
+    def split_match(self, match):
+        match, line, col, error, warning, message, near = (
+            super().split_match(match)
+        )
+        module = match.groupdict()['module']
+
+        return match, line, col, error, warning, module + " " + message, near
